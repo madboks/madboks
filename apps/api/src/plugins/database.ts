b8872fe04c6fd1dbbf3cfe5d5070a8d92c3dd65a
Jose Manuel Rosa Moncayo <josemanuel.rosamoncayo+github@gmail.com>
2024-04-05 23:04:01 +0200

    chore: dont change database schema

--- apps/api/src/plugins/database.ts
+++ apps/api/src/plugins/database.ts
@@ -2,7 +2,7 @@ import fp from 'fastify-plugin'
 import { FastifyPluginAsync } from 'fastify'
 import { PrismaClient } from '@prisma/client'
 
-export const database: FastifyPluginAsync = fp(async (server) => {
+const prismaPlugin: FastifyPluginAsync = fp(async (server) => {
   // const prisma = new PrismaClient()
 
   // await prisma.$connect()
@@ -13,3 +13,5 @@ export const database: FastifyPluginAsync = fp(async (server) => {
   //     await server.db.$disconnect()
   //   })
 })
+
+export default prismaPlugin

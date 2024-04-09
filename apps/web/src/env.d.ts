84cc96377e65bed06730f1c8bc1aca26eeca9854
Jose Manuel Rosa Moncayo <josemanuel.rosamoncayo+github@gmail.com>
2024-04-09 09:27:48 +0200

    fix: update env vars to be able to sue them in the frontend code

--- apps/web/src/env.d.ts
+++ apps/web/src/env.d.ts
@@ -2,6 +2,12 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly OAUTH_CLIENT_ID: string
-  readonly OAUTH_REDIRECT_URI: string
+  readonly HOST: string
+  readonly PORT: string
+  readonly PUBLIC_API_URL: string
+  readonly PUBLIC_APP_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
